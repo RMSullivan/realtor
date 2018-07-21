b2a74f1c7b3b8367776991d26a2010ff6875e056
github username <github email address>
2018-07-21 07:39:20 -0400

    review with Enoch
    remoce s from listing to solve issue with mutiple issues

--- realtor.rb
+++ realtor.rb
@@ -6,13 +6,13 @@ def scraper
   url = "https://www.realtor.com/apartments/33334"
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
-  rental_listings = parsed_page.css('li#1.component_property-card.js-component_property-card')
+  rental_listings = parsed_page.css('li.component_property-card.js-component_property-card')
   rental_listings.each do |rental_listing|
     listing = {
-      address: rental_listings.css('span.listing-street-address').text,
-      town: rental_listings.css('span.listing-city').text,
-      beds: rental_listings.css('span.data-value.meta-beds-display').text,
-      price: rental_listings.css('span.data-price').text
+      address: rental_listing.css('span.listing-street-address').text,
+      town: rental_listing.css('span.listing-city').text,
+      beds: rental_listing.css('span.data-value.meta-beds-display').text,
+      price: rental_listing.css('span.data-price').text
     }
     byebug
   end
