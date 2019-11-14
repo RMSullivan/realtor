0c22e28ba72cc947e15eb17f078c38fb4e335e7c
github username <github email address>
2019-11-14 11:37:31 -0500

    update gems

--- realtor.rb
+++ realtor.rb
@@ -7,27 +7,30 @@ def scraper
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   listings = Array.new
-  rental_listing = parsed_page.css('li.component_property-card.js-component_property-card')
+  rental_listings = parsed_page.css('li.component_property-card.js-component_property-card')
   page = 1
-  per_page = rental_listing.count #48
+  per_page = rental_listings.count #48
   total = parsed_page.css('span#search-result-count.page-title').text.tr("\n","").to_i
-  last_page = (total / per_page).round #2
+  last_page = 1 + (total / per_page).round #2
   while page <= last_page
      pagination_url = "https://www.realtor.com/apartments/33334/pg-#{page}"
      pagination_unparsed_page = HTTParty.get(pagination_url)
      pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
-    rental_listing.each do |rental_listing|
+     pagination_rental_listings = pagination_parsed_page.css('li.component_property-card.js-component_property-card')
+     pagination_rental_listings.each do |rental_listing|
       listing = {
         address: rental_listing.css('span.listing-street-address').text,
         town: rental_listing.css('span.listing-city').text,
         beds: rental_listing.css('span.data-value.meta-beds-display').text,
-        price: rental_listing.css('span.data-price').text
+        price: rental_listing.css('span.data-price').text,
       }
       listings << listing
+      puts listing
 
     end
+    page += 1
+  end
   byebug
 end
 
-end
 scraper
